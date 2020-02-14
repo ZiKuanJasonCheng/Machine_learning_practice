97a10db8930da76c6a336a3d5b417d704dd8dc9f
Zi-Kuan Cheng <40523038+ZiKuanJasonCheng@users.noreply.github.com>
2020-02-14 21:21:07 +0800

    Add files via upload
    Undone...

---
+++ transfer_learning/image_classification/train_target_model.py
@@ -0,0 +1,22 @@
+import transfer_img_classification
+
+if __name__ == "__main__":
+  img_height = 32
+  img_width = 32
+  path = "./images/ILSVRC/Data/CLS-LOC/train/"
+
+  # Load target data
+  train_c1_list = [('n02123045', 1), ('n02123159', 1), ('n02123394', 1)]  # Cats dataset
+  test_c1_list = train_c1_list
+  train_c0_list = [('n01440764', 0), ('n01443537', 0)]  # Other animals dataset
+  test_c0_list = train_c0_list
+  train_c1_interval = (0, 150)
+  train_c0_interval = (1000, 1225)
+  test_c1_interval = (150, 184)
+  test_c0_interval = (1225, 1275)
+  X_train, y_train, X_test, y_test = transfer_learning_github.read_data(path, train_c1_list, train_c1_interval, train_c0_list, train_c0_interval, test_c1_list,
+                                               test_c1_interval, test_c0_list, test_c0_interval)
+
+  load_ckpt_meta = './source_model/202002132209_model_epoch500.ckpt'  # Model checkpoint file path
+  dict_config = {'ckpt': load_ckpt_meta, 'init_scope': 'filters[12]', 'trainable_scope': 'filters3|dense[123]'}
+  transfer_learning_github.train_model(X_train, y_train, X_test, y_test, img_height, img_width, source=False, )
